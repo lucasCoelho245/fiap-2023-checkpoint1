ee042d82f3a6365fe79f0d905723407554953aff
Matheus Canovas <matheuscanovas9@gmail.com>
2023-03-13 23:38:22 -0300

    Criando projeto

---
+++ src/main/java/com/github/devcanovas/checkpoint1_api/Fiap2023Checkpoint1Application.java
@@ -0,0 +1,13 @@
+package com.github.devcanovas.checkpoint1_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fiap2023Checkpoint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fiap2023Checkpoint1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/devcanovas/checkpoint1_api/Fiap2023Checkpoint1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.devcanovas.checkpoint1_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fiap2023Checkpoint1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
